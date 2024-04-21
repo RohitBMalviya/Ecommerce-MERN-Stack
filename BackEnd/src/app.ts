85c2d937fd39c298067f6a5812e78fc6d121e81e
RohitMalviya <rohitbmalviya@gmail.com>
2024-04-21 17:44:13 +0530

    ....

--- BackEnd/src/app.ts
+++ BackEnd/src/app.ts
@@ -2,15 +2,17 @@ import express, { Express } from "express";
 import userRouter from "./routers/User/user.routes.js";
 import productRouter from "./routers/Product/product.routes.js";
 import { Error } from "./middleware/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app: Express = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ limit: "20kb" }));
+app.use(express.urlencoded({ extended: true, limit: "20kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use(Error);
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
-
-app.use(Error);
 
 export default app;
